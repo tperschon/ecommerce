37db579ea97e4f8c5e8e70fa2ea2a67230565b1e
TPerschon <timperschon@gmail.com>
2022-09-26 00:43:40 -0600

    Category routes implemented

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,74 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const handleError = require('../../utils/handleError');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// find all categories
+router.get('/', async (req, res) => {
+  try {
+    // get all categories including their products data
+    const categoryData = await Category.findAll({ include: { model: Product }});
+    // send status 200 and the data to client
+    res.status(200).json(categoryData);
+  } catch (err) {
+    handleError(req, res, err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// find one category by its `id` value, including its products data
+router.get('/:id', async (req, res) => {
+  try {
+    // get one category including its products
+    const singleCategory = await Category.findOne({ 
+      where: { id: req.params.id },
+      include: { model: Product }
+    });
+    // send status 200 and the data to client
+    res.status(200).json(singleCategory);
+  } catch (err) {
+    handleError(req, res, err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+    // create the category using req.body
+    const createdCategory = await Category.create(req.body);
+    // send status 200 and the data to client
+    res.status(200).json(createdCategory);
+  } catch (err) {
+    handleError(req, res, err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    // first find the category
+    const updatedCategory = await Category.findOne({ where: { id: req.params.id }});
+    // update the name with the req.body.category_name
+    updatedCategory.update({ category_name: req.body.category_name });
+    // send status 200 and the data to client
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    handleError(req, res, err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    // first find the category
+    const deletedCategory = await Category.findOne({ where: { id: req.params.id }});
+    // delete the category
+    deletedCategory.destroy();
+    // send status 200 and the data to client
+    res.status(200).json(deletedCategory);
+  } catch (err) {
+    handleError(req, res, err);
+  }
 });
 
 module.exports = router;
