fb377d669e0af370ec8be5c0be95a56037462a29
TPerschon <timperschon@gmail.com>
2022-09-25 22:10:26 -0600

    Models defined and relationships finished in index of models

--- models/index.js
+++ models/index.js
@@ -6,11 +6,12 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: ''
+  foreignKey: 'category_id'
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: ''
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, { through: ProductTag });
