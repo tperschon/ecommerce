ee8eedf0d65466bc37264de31ba38517e2877bc2
TPerschon <timperschon@gmail.com>
2022-09-26 01:16:42 -0600

    Fixing broken starter code route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -32,27 +32,22 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+router.post('/', async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    if (req.body.tagIds) {
+      const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: newProduct.id,
+          tag_id
+        };
+      });
+      const productTags = await ProductTag.bulkCreate(productTagIdArr);
+      res.status(200).json({ product: newProduct, tags: productTags });
+    } else res.status(200).json(newProduct);
+  } catch (err) {
+    handleError(req, res, err);
+  }
 });
 
 // update product
