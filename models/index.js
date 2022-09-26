7cdf4ebe3f137fafb91e4ae136377266bd3524ae
TPerschon <timperschon@gmail.com>
2022-09-25 21:51:05 -0600

    Some relationships defined

--- models/index.js
+++ models/index.js
@@ -5,12 +5,18 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: ''
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: ''
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
